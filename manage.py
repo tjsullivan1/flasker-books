d45e37ce629857767419f85db8f7851999644c40
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-07-09 16:37:37 -0500

    added book model

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 from flask.cli import FlaskGroup
 
 from src import create_app, db
-from src.api.models import User
+from src.api.models import User, Book
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,9 @@
 # src/api/models.py
 
-
+from enum import Enum
 from sqlalchemy.sql import func
+from sqlalchemy import Column, Text, String, DateTime, Enum as SQLEnum, CheckConstraint, Integer
+from uuid import uuid4
 
 from src import db
 
@@ -19,3 +21,45 @@ class User(db.Model):
     def __init__(self, username, email):
         self.username = username
         self.email = email
+
+
+class PriorityLevel(Enum):
+    HIGH = "high"
+    MEDIUM = "medium"
+    LOW = "low"
+
+
+class ReadingStatus(Enum):
+    TO_READ = "to_read"
+    READING = "reading"
+    READ = "read"
+
+
+class BookType(Enum):
+    PHYSICAL = "physical"
+    EBOOK = "ebook"
+    AUDIOBOOK = "audiobook"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = Column(String, primary_key=True, default=uuid4)
+    title = Column(Text)
+    author = Column(Text)
+    genre = Column(Text)
+    date_added = Column(DateTime, default=func.now())
+    priority = Column(SQLEnum(PriorityLevel), default=PriorityLevel.LOW, nullable=False)
+    referred_by = Column(Text)
+    status = Column(SQLEnum(ReadingStatus), default=ReadingStatus.TO_READ, nullable=False)
+    category =  Column(Text)
+    notes = Column(Text)
+    type_read = Column(SQLEnum(BookType), default=BookType.AUDIOBOOK, nullable=False)
+    rating = Column(Integer, CheckConstraint('rating >= 1 AND rating <= 5'))
+    date_read = Column(DateTime)
+
+    __table_args__ = (
+        CheckConstraint('rating >= 1 AND rating <= 5', name='rating_range'),
+    )
+
+
