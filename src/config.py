6fb0d08cfb7f8661be716a89c361bfdd0567a65e
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-07-23 13:35:24 -0500

    updating version of gunicorn

--- src/config.py
+++ src/config.py
@@ -24,3 +24,5 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
